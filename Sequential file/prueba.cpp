8c06c8b66f27be9d106adfa7b510b49e9b5f3620
Leonardo Isidro <leoisidro@MacBook-Air-de-Leonardo.local>
2023-09-03 01:18:20 -0500

    implementacion del insert y el rangesearch, falta pruebas

---
+++ Sequential file/prueba.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string a = "Ana";
+    string b = "Leo";
+
+    if (a < b) {
+        cout << "a es menor que b" << endl;
+    } else {
+        cout << "a no es menor que b" << endl;
+    }
+
+    return 0;
+}
--- Sequential file/sequential_file.cpp
+++ Sequential file/sequential_file.cpp
@@ -10,13 +10,34 @@ struct Record {
     char surname[20];
     int ciclo;
     int next;
+    char archivo;
+
+    void setData();
 };
+
+void Record::setData() {
+    cout << "ID: ";
+    cin >> id;
+    cout << "Name: ";
+    cin >> name;
+    cout << "Surname: ";
+    cin >> surname;
+    cout << "Ciclo: ";
+    cin >> ciclo;
+    next = -1;
+    archivo = 'd';
+}
+
+
 
 template <class T>
 class SequentialFile{
-    string filename;
+    string datos;
+    string auxiliar;
 public:
-    SequentialFile(string filename);
+    SequentialFile(string filename1,string filename2);
+    int size_datos();
+    int size_auxiliar();
     bool insert(Record record);
     bool remove(T key);
     vector<Record> search(T key);
@@ -24,51 +45,122 @@ public:
 };
 
 template <class T>
-SequentialFile<T>::SequentialFile(string filename) {
-    this->filename = filename;
+SequentialFile<T>::SequentialFile(string filename1,string filename2) {
+    this->datos = filename1;
+    this->auxiliar = filename2;
+}
+
+template <class T>
+int SequentialFile<T>::size_datos() {
+    fstream file(this->datos, ios::in | ios::binary);
+    if (!file.is_open()) throw ("No se pudo abrir el archivo");
+    file.seekg(0, ios::end);
+    int size = file.tellg();
+    file.close();
+    return size / sizeof(Record);
+}
+
+template <class T>
+int SequentialFile<T>::size_auxiliar() {
+    fstream file(this->auxiliar, ios::in | ios::binary);
+    if (!file.is_open()) throw ("No se pudo abrir el archivo");
+    file.seekg(0, ios::end);
+    int size = file.tellg();
+    file.close();
+    return size / sizeof(Record);
 }
 
 template <class T>
 bool SequentialFile<T>::insert(Record record) {
-    /*
-    fstream file;
-    file.open(filename, ios::in | ios::out | ios::binary);
-    if (!file.is_open()) {
-        file.open(filename, ios::out | ios::binary);
+
+    fstream file(this->datos, ios::app | ios::binary | ios::in | ios::out);
+
+    if(!file.is_open()) throw ("No se pudo abrir el archivo");
+    
+    if(size() == 0){
+        // separar en caso no funcione
+        file.seekp(0, ios::end);
         file.write((char*)&record, sizeof(record));
         file.close();
         return true;
     }
-    file.seekg(0, ios::end);
-    int size = file.tellg();
-    file.seekg(0, ios::beg);
-    Record last;
-    file.read((char*)&last, sizeof(last));
-    if (last.id < record.id) {
-        file.seekg(0, ios::end);
-        file.write((char*)&record, sizeof(record));
-        file.close();
-        return true;
-    }
-    int pos = 0;
-    while (pos < size) {
-        Record current;
-        file.read((char*)&current, sizeof(current));
-        if (current.id == record.id) {
-            file.close();
-            return false;
-        }
-        if (current.id > record.id) {
-            file.seekg(pos, ios::beg);
+    else{
+        file.seekg(-sizeof(record), ios::end);
+        Record last;
+        file.read((char*)&last, sizeof(last));
+    
+        if(record.name>last.name){
+            last.next = size_datos();
+            file.seekp(-sizeof(record), ios::end);
+            file.write((char*)&last, sizeof(last));
+            file.seekp(0,ios::cur);
             file.write((char*)&record, sizeof(record));
-            file.write((char*)&current, sizeof(current));
             file.close();
             return true;
         }
-        pos += sizeof(current);
+        else{
+            fstream aux(this->auxiliar, ios::app | ios::binary | ios::in | ios::out);
+            if(!aux.is_open()) throw ("No se pudo abrir el archivo");
+
+            Record current;
+            file.seekg(0, ios::beg);
+            file.read((char*)&current, sizeof(current));
+            int pos = 0;
+
+            while (record.name>current.name)
+            {
+                file.read((char*)&current, sizeof(current));
+                pos++;
+            }
+
+            file.seekg((pos-1)*sizeof(record), ios::beg);
+            file.read((char*)&current, sizeof(current));
+            int siguiente = current.next;
+            char archivo = current.archivo;
+            
+            if(current.archivo == 'a'){
+                Record auxiliar;
+                aux.seekg(siguiente*sizeof(Record), ios::beg);
+                aux.read((char*)&auxiliar, sizeof(auxiliar));
+                int anterior = siguiente;
+                while(record.name>auxiliar.name && auxiliar.archivo == 'a'){
+                    aux.seekg(siguiente*sizeof(Record), ios::beg);
+                    aux.read((char*)&auxiliar, sizeof(auxiliar));
+                    anterior = siguiente;
+                    siguiente = auxiliar.next;
+                    archivo = auxiliar.archivo;
+                }
+
+                record.archivo = archivo;
+                record.next = siguiente;
+                auxiliar.next = size_auxiliar();
+                aux.seekp(0, ios::end);
+                aux.write((char*)&record, sizeof(record));
+                aux.seekp(anterior*sizeof(Record), ios::beg);
+                aux.write((char*)&auxiliar, sizeof(auxiliar));
+                aux.close();
+                file.close();
+                return true;
+            }
+            else{
+                    current.next = size_auxiliar();
+                    current.archivo = 'a';
+                    file.seekp((pos-1)*sizeof(record), ios::beg);
+                    file.write((char*)&current, sizeof(current));
+                    record.archivo = archivo;
+                    record.next = siguiente; 
+                    aux.seekp(0, ios::end);
+                    aux.write((char*)&record, sizeof(record));
+                    aux.close();
+                    file.close();
+                    return true;
+            
+            }
+            fiel.close();
+        }
     }
-    file.close();
-    return false; */  // recomendacion de copilot
+
+    
 }
 
 template <class T>
@@ -128,26 +220,49 @@ vector<Record> SequentialFile<T>::search(T key) {
 
 template <class T>
 vector<Record> SequentialFile<T>::range_search(T key1, T key2) {
-    /*
     vector<Record> result;
-    fstream file;
-    file.open(filename, ios::in | ios::binary);
-    if (!file.is_open()) {
+    fstream file(this->datos, ios::in | ios::binary| ios::out);
+    fstream aux(this->auxiliar, ios::in | ios::binary| ios::out);
+
+    if (!file.is_open() || !aux.is_open()) {
         return result;
     }
-    file.seekg(0, ios::end);
-    int size = file.tellg();
-    file.seekg(0, ios::beg);
+
+    char archivo;
     int pos = 0;
-    while (pos < size) {
+
+    file.seekg(0, ios::beg);
+    file.read((char*)&archivo, sizeof(archivo));
+    file.read((char*)&pos, sizeof(pos));
+
+
+   if(archivo=='d'){
         Record current;
+        file.seekg(pos*sizeof(Record), ios::beg);
         file.read((char*)&current, sizeof(current));
-        if (current.id >= key1 && current.id <= key2) {
-            result.push_back(current);
+        while (current.name<=key2)
+        {
+            if(current.name>=key1){
+                result.push_back(current);
+            }
+            file.read((char*)&current, sizeof(current));
         }
-        pos += sizeof(current);
+        file.close();
+        return result;
     }
-    file.close();
-    return result;
-    */ // recomendacion de copilot
+    else{
+        Record current;
+        aux.seekg(pos*sizeof(Record), ios::beg);
+        aux.read((char*)&current, sizeof(current));
+        while (current.name<=key2)
+        {
+            if(current.name>=key1){
+                result.push_back(current);
+            }
+            aux.read((char*)&current, sizeof(current));
+        }
+        aux.close();
+        return result;
+    }
+
 }
